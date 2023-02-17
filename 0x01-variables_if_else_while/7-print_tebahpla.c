49b3d418de4a4139529d983af284a1cb2a6ba28f
ThapeloPanda <thapeloramathetse@gmail.com>
2023-02-17 15:13:59 -0800

    print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: Thapelo Ramathetse
+ */
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
