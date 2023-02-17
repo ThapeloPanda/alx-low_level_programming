9e6622f4b43d9ed4053271c4816466a7d44025d2
ThapeloPanda <thapeloramathetse@gmail.com>
2023-02-16 17:25:37 -0800

    printing a line of text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * int main - put '"Programmingis is like building a multilingual puzzle
+ * Return: 0
+ */
+
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle\n");
+	puts('"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
