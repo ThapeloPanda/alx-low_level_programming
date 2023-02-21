59fd1391acf32268e817fc348bcf7930702c2637
ThapeloPanda <thapeloramathetse@gmail.com>
2023-02-21 15:24:46 -0800

    a+b file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds twovalues
+ * @n1: The first number input
+ * @n2: The second number input
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
